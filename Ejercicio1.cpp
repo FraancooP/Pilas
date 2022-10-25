93c8279a98399f8f6fdb70ec4cd2ab43c6e1d89b
franco <Fran>
2022-10-25 10:46:49 -0300

    Ejercicio 1 sin terminar

---
+++ Ejercicio1.cpp
@@ -0,0 +1,15 @@
+/*Crear un programa con el siguiente menu de opciones: 
+
+Agregar un nodo a la pila. (se deben agregar 5 valores)
+Borrar el último nodo de la pila.
+Imprimir pila.
+Suma de los valores de la pila.*/
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]){
+
+
+    
+    system("pause");
+    return 0;
+}
